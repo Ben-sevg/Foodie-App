bf251ab73069ee13a15b1debd79d6241443487bb
Willie-Conway <hire.willie.conway@gmail.com>
2025-03-29 01:45:21 -0400

    Initial commit of Foodie app

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -26,7 +26,7 @@ export default function WelcomeScreen() {
       300
     );
 
-    setTimeout(() => navigation.navigate("Home"), 2500);
+    setTimeout(() => navigation.navigate("Home"), 1200);
   }, []);
 
   return (
